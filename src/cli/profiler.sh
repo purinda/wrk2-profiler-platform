7f800b45f7d434505f0ef06ce5325bb7f72a2a61
Purinda Gunasekara <purinda@gmail.com>
2018-08-07 23:35:16 +1000

    Implemented logger functionality

--- src/cli/profiler.sh
+++ src/cli/profiler.sh
@@ -4,25 +4,22 @@ source "${app}/src/cli/logger.sh"
 source "${app}/src/cli/yaml.sh"
 
 function profile() {
+    total=0
     profiler_fn=$1
 
     # Parse the input yaml file
     create_variables $profiler_fn
-
-    note "Running profile '$profiler_fn'"
+    out "Running profile '$profiler_fn'"
 
     if [ "${debug}" == "1" ]; then
-        console "Threads:     $profiler_threads"
-        console "Connections: $profiler_connections"
-        console "Duration:    $profiler_duration"
-        console "Rate:        $profiler_rate"
-        console ""
+        out "Threads:     $profiler_threads"
+        out "Connections: $profiler_connections"
+        out "Duration:    $profiler_duration"
+        out "Rate:        $profiler_rate"
+        out ""
     fi
 
-    total=0
-    timestamp=$(date +%Y%m%d_%H%M)
-    output_dir=/tmp/results/${name}${timestamp}
-    mkdir -p ${output_dir}
+    mkdir_results
 
     # Run wrk for each URL in the profile
     for idx in $( seq 0 ${calls__active[@]} )
@@ -32,33 +29,47 @@ function profile() {
         fi
 
         # If authentication is required
-        if [ "$auth" == "1" ] && [ -z ${AUTHTOKEN+x} ]; then
-            console "AuthToken is not set. Skip request needs authentication"
+        if [ "$auth" == "1" ] && [ -z ${auth_token+x} ]; then
+            err "AuthToken is not set. Skip request needs authentication"
             continue
         fi
 
-        note "Testing '${calls__name[$idx]}'"
-        cmd="wrk -t${profiler_threads} -c${profiler_connections} 
-            -d${profiler_duration} -R${profiler_rate} -s ${calls__script[$idx]} 
-            --latency ${base_url}${calls__path[$idx]}"
+        out_n "Testing '${calls__name[$idx]}'.."
+        cmd="wrk -t${profiler_threads} -c${profiler_connections} -d${profiler_duration} -R${profiler_rate} -s${calls__script[$idx]} --latency ${base_url}${calls__path[$idx]}"
+        eval ${cmd} > ${output_dir}/${calls__name[$idx]}.log
+        is_ok
 
         if [ "${debug}" == "1" ]; then
-            console "URL: ${base_url}${calls__path[$idx]}"
-            console "Shell Cmd: ${cmd}"
+            out "URL: ${base_url}${calls__path[$idx]}"
+            out "Cmd: ${cmd}"
         fi
 
-        eval ${cmd} > ${output_dir}/${calls__name[$idx]}.log
-        console ""
+        out ""
         ((total++))
     done
 
-    note "${total} request(s) tested in total"
+    out "${total} request(s) tested in total"
+    generate_histogram
 
+    out "Results in ${output_dir}"
+    out "Done"
+}
+
+function mkdir_results() {
+    out_n "Setting up results directory.."
+    timestamp=$(date +%Y%m%d_%H%M)
+    output_dir=/tmp/results/${name}_${timestamp}
+    mkdir -p ${output_dir}
+    is_ok
+    out ""
+}
+
+function generate_histogram() {
     # Histogram
     if [ "${histogram}" == "0" ]; then
         note "Skip drawing HdrHistogram as result is not saved."
     else
-        note "Producing histogram.."
+        out_n "Producing histogram.."
         input=""
         for result in ${output_dir}/*
         do
@@ -66,8 +77,7 @@ function profile() {
         done
 
         # Run gnuplot
-        ${app}/src/cli/make_percentile_plot -o ${output_dir}/histogram.svg $input
+        ${app}/src/cli/make_percentile_plot -o ${output_dir}/histogram.svg $input &> /dev/null
+        is_ok
     fi
-
-    console "Done"
 }
