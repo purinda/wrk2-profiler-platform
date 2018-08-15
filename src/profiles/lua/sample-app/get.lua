95e28e423f57f7cd976ff30fe80e8c52f39c3741
Purinda Gunasekara <purinda.gunasekara@openbet.com>
2018-08-15 15:31:48 +1000

    Reverted back to wrk2 after wrk not producing detailed histogram data

--- src/profiles/lua/sample-app/get.lua
+++ src/profiles/lua/sample-app/get.lua
@@ -1,4 +1,13 @@
 -- HTTP method, body, and adding a header
+-- done() function that prints latency percentiles as CSV
 
 wrk.method = "GET"
 wrk.headers["Content-Type"] = "application/json"
+
+done = function(summary, latency, requests)
+    io.write("------------------------------\n")
+    for _, p in pairs({ 50, 90, 99, 99.999 }) do
+       n = latency:percentile(p)
+       io.write(string.format("%g%%,%d\n", p, n))
+    end
+ end
